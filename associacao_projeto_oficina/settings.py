ea7c781f08963ebfa8b2b4da5e8bfb8dff415b3b
MauricioDiasSilva <2204111@aluno.univesp.br>
2024-07-28 16:42:48 -0300

    alteraçãosettings

--- associacao_projeto_oficina/settings.py
+++ associacao_projeto_oficina/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         'NAME': 'db',
         'USER': 'Mauricio',
         'PASSWORD': 'Dud@cl@r@01',
-        'HOST': 'projeto-integrador-1.mysql.database.azure.com',
+        'HOST': 'associacao-projeto-oficina-1.mysql.database.azure.com',
         'PORT': '3306',
         # 'OPTIONS': {
         #     'ssl': {
